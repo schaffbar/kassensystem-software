38bb9a01bcc760229f95ea42a2b8e75172b7d886
pkrantz <pkrantz@users.noreply.github.com>
2025-09-19 21:36:04 +0200

    add docker compose file

---
+++ schaffbar-backend/infra/database/init.sql
@@ -0,0 +1,9 @@
+-- Database initialization
+
+-- Create a new database user
+--CREATE USER ${POS_DB_USER} WITH ENCRYPTED PASSWORD ${POS_DB_PASSWORD};
+
+-- Create schema
+CREATE SCHEMA IF NOT EXISTS schaffbar;
+
+-- Add more tables as needed
