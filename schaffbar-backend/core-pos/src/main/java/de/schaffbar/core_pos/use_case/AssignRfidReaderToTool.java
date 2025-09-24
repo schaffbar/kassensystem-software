77ab8c9422482cb38f7eab4baae854b99d9e786e
pkrantz <pkrantz@users.noreply.github.com>
2025-09-24 18:21:32 +0200

    add assign RFID reader to tool use case

---
+++ schaffbar-backend/core-pos/src/main/java/de/schaffbar/core_pos/use_case/AssignRfidReaderToTool.java
@@ -0,0 +1,37 @@
+package de.schaffbar.core_pos.use_case;
+
+import de.schaffbar.core_pos.ResourceNotFoundException;
+import de.schaffbar.core_pos.RfidReaderId;
+import de.schaffbar.core_pos.ToolId;
+import de.schaffbar.core_pos.rfid_reader.RfidReaderService;
+import de.schaffbar.core_pos.rfid_reader.RfidReaderType;
+import de.schaffbar.core_pos.rfid_reader.RfidReaderViews.RfidReaderView;
+import de.schaffbar.core_pos.tool.ToolService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@Service
+@Validated
+@RequiredArgsConstructor
+public class AssignRfidReaderToTool {
+
+    private final @NonNull RfidReaderService rfidReaderService;
+
+    private final @NonNull ToolService toolService;
+
+    public void process(@NotNull @Valid ToolId toolId, @NotNull @Valid RfidReaderId rfidReaderId) {
+        RfidReaderView rfidReader = this.rfidReaderService.getRfidReader(rfidReaderId) //
+                .orElseThrow(() -> ResourceNotFoundException.rfidReader(rfidReaderId));
+
+        if (rfidReader.type() != RfidReaderType.S) {
+            throw new RuntimeException("TODO: Invalid type of RFID reader");
+        }
+
+        this.toolService.assignRfidReader(toolId, rfidReaderId);
+    }
+
+}
