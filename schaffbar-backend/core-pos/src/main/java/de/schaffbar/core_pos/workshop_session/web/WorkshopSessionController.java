a1dad83f11250fb631ac436e62c63d7e8e39836b
pkrantz <pkrantz@users.noreply.github.com>
2025-11-08 17:59:08 +0100

    [backend] remove unused interface

--- schaffbar-backend/core-pos/src/main/java/de/schaffbar/core_pos/workshop_session/web/WorkshopSessionController.java
+++ schaffbar-backend/core-pos/src/main/java/de/schaffbar/core_pos/workshop_session/web/WorkshopSessionController.java
@@ -38,22 +38,6 @@ public class WorkshopSessionController {
     // ------------------------------------------------------------------------
     // query
 
-    @GetMapping(value = "open/{customerId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<WorkshopSessionApiDto> getOpenWorkshopSession(@PathVariable @NotNull CustomerId customerId) {
-        WorkshopSessionView openSession = this.workshopSessionService.getOpenWorkshopSession(customerId) //
-                .orElse(null);
-
-        if (isNull(openSession)) {
-            return ResponseEntity.ok(null);
-        }
-
-        List<WorkshopUsageView> usages = this.workshopUsageService.getWorkshopUsages(openSession.id());
-
-        WorkshopSessionApiDto result = WorkshopSessionApiMapper.MAPPER.toWorkshopSessionApiDto(openSession, usages);
-
-        return ResponseEntity.ok(result);
-    }
-
     @GetMapping(value = "{customerId}/active", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<WorkshopSessionApiDto> getActiveWorkshopSession(@PathVariable @NotNull CustomerId customerId) {
         WorkshopSessionView openSession = this.workshopSessionService.getOpenWorkshopSession(customerId) //
