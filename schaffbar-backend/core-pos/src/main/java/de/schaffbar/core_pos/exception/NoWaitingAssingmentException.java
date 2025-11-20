08d82e7cfbf15ea9d82d64b0596d27ec27194b19
pkrantz <pkrantz@users.noreply.github.com>
2025-11-20 21:09:00 +0100

    [backend] fix error handling

--- schaffbar-backend/core-pos/src/main/java/de/schaffbar/core_pos/exception/NoWaitingAssingmentException.java
+++ schaffbar-backend/core-pos/src/main/java/de/schaffbar/core_pos/exception/NoWaitingAssingmentException.java
@@ -9,7 +9,7 @@ public class NoWaitingAssingmentException extends RuntimeException {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    private static final String MESSAGE = "RFID [id: %s] nicht zugewiesen";
+    private static final String MESSAGE = "RFID nicht zugewiesen"; // TODO: improve message
 
     public NoWaitingAssingmentException(RfidTagId id) {
         super(String.format(MESSAGE, id.getValue()), null);
