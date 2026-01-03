2dd5ab2f3fd1b779b5a946a9a4143b61085f7b02
pkrantz <pkrantz@users.noreply.github.com>
2026-01-03 17:31:55 +0100

    add i18n for user open session

--- schaffbar-portal/src/app/users/pages/user-open-session/user-open-session.component.ts
+++ schaffbar-portal/src/app/users/pages/user-open-session/user-open-session.component.ts
@@ -4,13 +4,15 @@ import { Component, inject, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 
+import { TranslatePipe } from '@ngx-translate/core';
+
 import { UserDetailStore } from '../user-detail/user-detail.store';
 
 @Component({
   selector: 'schbar-user-open-session',
   templateUrl: './user-open-session.component.html',
   styleUrl: './user-open-session.component.scss',
-  imports: [MatTableModule, MatButtonModule, DatePipe],
+  imports: [MatTableModule, MatButtonModule, DatePipe, TranslatePipe],
 })
 export class UserOpenSessionComponent {
   protected readonly detailsStore = inject(UserDetailStore);
