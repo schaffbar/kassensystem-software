4d9964e6603930496144df595689935afeff07d6
pkrantz <pkrantz@users.noreply.github.com>
2026-04-18 23:30:24 +0200

    [backend] add interface to list all active tools

---
+++ schaffbar-backend/core-pos/src/main/java/de/schaffbar/core_pos/tool_dashboard/web/ToolDashboardController.java
@@ -0,0 +1,36 @@
+package de.schaffbar.core_pos.tool_dashboard.web;
+
+import java.util.List;
+
+import de.schaffbar.core_pos.tool_dashboard.ToolDashboardService;
+import de.schaffbar.core_pos.tool_dashboard.web.ToolDashboardApiModel.ToolDashboardEntryApiDto;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/api/v1/tool-dashboard")
+public class ToolDashboardController {
+
+    private final @NonNull ToolDashboardService toolDashboardService;
+
+    // ------------------------------------------------------------------------
+    // query
+
+    @GetMapping(value = "/active-tool-usages", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<ToolDashboardEntryApiDto>> getActiveToolUsages() {
+        List<ToolDashboardEntryApiDto> activeToolUsages = this.toolDashboardService.getActiveToolUsages().stream() //
+                .map(ToolDashboardApiModel.MAPPER::toToolDashboardEntryApiDto) //
+                .toList();
+
+        return ResponseEntity.ok(activeToolUsages);
+    }
+
+}
